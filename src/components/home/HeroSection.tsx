97e26d6f27c8db2b11bba69cd7a6f323477f006a
alejandro_rey <areyt01@iesalbarregas.es>
2026-02-18 17:37:19 +0100

    ✅Traductor

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,4 +1,5 @@
 import { useBearStore } from "../../stores/store";
+import { useTranslation } from "react-i18next";
 
 const HeroSection = () => {
     const bears = useBearStore(state => state.bears)
@@ -6,10 +7,12 @@ const HeroSection = () => {
     const feed = useBearStore(state => state.feed)
     const increasePopulation = useBearStore(state => state.increasePopulation)
 
+    const { t } = useTranslation();
+
     return (
         <section style={{ textAlign: 'center', padding: '4rem 2rem' }}>
-            <h1>Bienvenido a nuestra tienda</h1>
-            <p>Explora nuestros productos</p>
+            <h1>{t('heroSection.tittle')}</h1>
+            <p>{t('heroSection.description')}</p>
 
             <div>
                 <p>Osos: {bears}</p>
