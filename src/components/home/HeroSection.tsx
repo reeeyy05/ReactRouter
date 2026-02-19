a1759d74c05451e2f55aca931a6a857cb169e77d
alejandro_rey <areyt01@iesalbarregas.es>
2026-02-19 19:53:08 +0100

    ✅Animacion

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,5 +1,6 @@
 import { useBearStore } from "../../stores/store";
 import { useTranslation } from "react-i18next";
+import Antigravity from "../animations/Antigravity";
 
 const HeroSection = () => {
     const bears = useBearStore(state => state.bears)
@@ -13,6 +14,26 @@ const HeroSection = () => {
         <section style={{ textAlign: 'center', padding: '4rem 2rem' }}>
             <h1>{t('heroSection.tittle')}</h1>
             <p>{t('heroSection.description')}</p>
+
+            <div style={{ width: '100%', height: '400px', position: 'relative' }}>
+                <Antigravity
+                    count={300}
+                    magnetRadius={5}
+                    ringRadius={5}
+                    waveSpeed={0.4}
+                    waveAmplitude={1}
+                    particleSize={1.5}
+                    lerpSpeed={0.05}
+                    color="#F6339A"
+                    autoAnimate
+                    particleVariance={1}
+                    rotationSpeed={0}
+                    depthFactor={1}
+                    pulseSpeed={3}
+                    particleShape="tetrahedron"
+                    fieldStrength={10}
+                />
+            </div>
 
             <div>
                 <p>Osos: {bears}</p>
